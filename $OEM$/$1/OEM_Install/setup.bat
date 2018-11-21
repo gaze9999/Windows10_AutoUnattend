3789ecd5853ead7a6d2728a350f45f3c86e66a90
gaze9999 <gaze9999@livemail.tw>
2018-11-21 13:47:55 +0800

    update_apps

--- $OEM$/$1/OEM_Install/setup.bat
+++ $OEM$/$1/OEM_Install/setup.bat
@@ -10,7 +10,7 @@ timeout /t 3 /nobreak
 ECHO.
 ECHO ====================== Installing JAVA8 =========================
 ECHO.
-%SYSTEMDRIVE%\OEM_Install\jre-8u181-windows-x64.exe /s
+%SYSTEMDRIVE%\OEM_Install\jre-8u191-windows-x64.exe /s
 timeout /t 3 /nobreak
 
 ECHO.
@@ -23,6 +23,12 @@ ECHO.
 ECHO ======== Installing Visual C++ Redistributable Packages =========
 ECHO.
 %SYSTEMDRIVE%\OEM_Install\vc_redist_x64.exe /S /install /quiet /norestart
+timeout /t 3 /nobreak
+
+ECHO.
+ECHO ======== Installing .Net 4.7.2 Redistributable Packages =========
+ECHO.
+%SYSTEMDRIVE%\OEM_Install\dotNetFx472_Full_x86-x64.exe /S /install /quiet /norestart
 timeout /t 3 /nobreak
 
 ECHO.
